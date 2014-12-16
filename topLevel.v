e2b0c44dc7de5cf827051c6d392488ad31d0da5d
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2014-12-16 00:24:56 -0500

    duh

--- topLevel.v
+++ topLevel.v
@@ -1,72 +1,78 @@
 module topLevel(clk, sw, btn, out, led);
+
 input[7:0] sw;
 input btn;
+wire[2:0] switchControlSignal;
+wire[2:0] noteControlSignal;
 wire[2:0] controlSignal;
-wire btnPosEdge, btnNegEdge, note;
-wire [6:0] noteOfSong;
+wire btnPos, btnNeg;
+reg songSelect = 0;
+
+wire strummerPos, strummerNeg;
 input clk;
 
 output out;
 output[7:0] led;
 
-inputconditioner btnConditioner(clk, btn, btnPosEdge, btnNegEdge);
+inputconditioner btnCond(clk, btn, btnPos, btnNeg);
 
 always @(posedge clk) begin
-	if(btnPosEdge) begin
-		songStored songStored(clk, noteOfSong);
-		guitar guitar(clk, noteOfSong, note);
-	end else begin
-		guitar guitar(clk, sw, note);
-	end
-
-
-
-
-assign out = note;
-
-end 
-
-endmodule
-
-
-module guitar(clk, switches, out);
-
-input[7:0] switches;
-wire[2:0] controlSignal;
-wire strummerPos, strummerNeg, btnPosEdge, btnNegEdge;
-input clk;
-
-output out;
-
-inputconditioner conditioner(clk, switches[0], strummerPos, strummerNeg);
-controlSignalGen control(clk, switches[7:1], strummerPos, strummerNeg, controlSignal);
-frequencyGen frequency(clk, controlSignal, out);
-
-
-endmodule
-
-module songStored(clk, note);
-input clk;
-output note;
-reg[31:0] time;
-reg[6:0] musicSheet [100:0];
-reg[100:0] index;
-
-
-// how are we gonna parse the notes?
-
-initial $readmemb("whateverWeNameTheFile.mem", musicSheet);
-
-always @(posedge clk) begin
-	if (time == 0) begin
-		index <= index + 1;
-		time <= 100000000
-	end else begin
-		time <= time + 1;
+	if (btnPos) begin
+		songSelect = ~songSelect;
 	end
 end
 
-assign note = musicSheet[index]
+inputconditioner conditioner(clk, sw[0], strummerPos, strummerNeg);
+controlSignalGen control(clk, sw[7:1], strummerPos, strummerNeg, switchControlSignal, led[6:0]);
+songGenerator songGen(clk, noteControlSignal);
+mux3bit mux(switchControlSignal, noteControlSignal, songSelect, controlSignal);
+frequencyGen frequency(clk, controlSignal, out);
+
+assign led[7] = songSelect;
+
+endmodule
+
+module songGenerator(clk, note);
+input clk;
+output[2:0] note;
+
+reg[31:0] t = 0;
+reg[30:0] musicSheet;
+reg[100:0] index = 0;
+
+parameter A = 3'd0;
+parameter B = 3'd1;
+parameter C = 3'd2;
+parameter D = 3'd3;
+parameter E = 3'd4;
+parameter F = 3'd5;
+parameter G = 3'd6;
+parameter none = 3'd7;
+
+
+initial musicSheet = {A, B, C, D, E, F, G};
+
+// how are we gonna parse the notes?
+//initial $readmemh("whateverWeNameTheFile.mem", musicSheet);
+
+always @(posedge clk) begin
+	if (t == 0) begin
+		if (index == 6) begin
+			index <= 0;
+		end
+		else begin 
+			index <= index + 3;
+			t <= 25000000;
+		end
+	end else begin
+		t <= t + 1;
+	end
+end
+
+
+assign note[2] = musicSheet[index];
+assign note[1] = musicSheet[index+1];
+assign note[0] = musicSheet[index+2];
 
 endmodule
 
@@ -75,7 +81,7 @@ module controlSignalGen(clk, switches, strummerPos, strummerNeg, controlSignal, 
 input[6:0] switches;
 input strummerPos, strummerNeg, clk;
 output reg[2:0] controlSignal = 0;
-output reg[7:0] led = 1;
+output reg[6:0] led = 0;
 
 wire strummerEdge;
 
@@ -83,8 +89,8 @@ or orgate(strummerEdge, strummerPos, strummerNeg);
 
 always @(posedge clk) begin
 	if (strummerEdge) begin
-		controlSignal = 6;
-		led = {1'b0, switches};
+		controlSignal = 7;
+		led = {switches};
 		case(switches)
 			7'b1000000: begin
 				controlSignal = 6;
@@ -128,8 +134,9 @@ parameter D = 3'd3;
 parameter E = 3'd4;
 parameter F = 3'd5;
 parameter G = 3'd6;
+parameter none = 3'd7;
 
-parameter clkSpeed = 25; //megahertz
+parameter clkSpeed = 50; //megahertz
 parameter aFreq = 220;
 parameter bFreq = 247;
 parameter cFreq = 261;
@@ -137,8 +144,6 @@ parameter dFreq = 294;
 parameter eFreq = 330;
 parameter fFreq = 349;
 parameter gFreq = 392;
-parameter selectedFreq = A;
-
 
 always @(posedge clk) begin
 	if (counter == 0) begin
@@ -169,6 +174,9 @@ always @(posedge clk) begin
 		G: begin
 			clkDivider = clkSpeed*1000000/gFreq/2;
 		end
+		//none: begin
+		//	clkDivider = clkSpeed; // generates a 1Hz freq, which we can't here
+		//end
 	endcase
 end
 endmodule
@@ -179,8 +187,8 @@ output reg positiveedge = 0;
 output reg negativeedge = 0;
 input clk, noisysignal;
 
-parameter counterwidth = 3;
-parameter waittime = 3;
+parameter counterwidth = 7;
+parameter waittime = 100;
 
 reg[counterwidth-1:0] counter =0;
 reg synchronizer0 = 0;
@@ -206,4 +214,18 @@ always @(posedge clk ) begin
     synchronizer1 = synchronizer0;
     synchronizer0 = noisysignal;
 end
+endmodule
+
+module mux3bit(input0, input1, control, out);
+
+input[2:0] input0, input1;
+input control;
+output[2:0] out;
+
+wire[2:0] mux_input[1:0]; // Creates a 2d Array of wires
+
+assign mux_input[0] = input0; // Connects the sources of the array
+assign mux_input[1] = input1;
+assign out = mux_input[control]; // Connects the output of the array
+
 endmodule
